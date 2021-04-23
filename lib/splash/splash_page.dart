db2c8771a51007705d42a11054949351b68c58c1
Juliana Alba <jualba.ti@gmail.com>
2021-04-23 19:21:20 -0300

    Criado tela inicial de interrogação splash_page #AtrasadoAula2

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/app_images.dart';
-import 'package:devquiz/home/home_page.dart';
-
-import 'package:flutter/material.dart';
-
 import '../core/app_gradients.dart';
 import '../core/app_images.dart';
 
